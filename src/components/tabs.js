bdf277e29096b71683078fdcc47a5070e8e70062
Jamaria Sims <75871993+UnloadedZero@users.noreply.github.com>
2021-07-09 18:07:44 -0400

    Task 3&4 Done

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,28 +1,24 @@
+import axios from "axios";
+
 const Tabs = (topics) => {
-  // TASK 3
-  // ---------------------
-  // Implement this function which takes an array of strings ("topics") as its only argument.
-  // As an example, if the topics passed are ['javascript', 'bootstrap', 'technology']
-  // then the function returns the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  // <div class="topics">
-  //   <div class="tab">javascript</div>
-  //   <div class="tab">bootstrap</div>
-  //   <div class="tab">technology</div>
-  // </div>
-  //
-}
+	const topicsHolder = document.createElement("div");
+	topicsHolder.classList = "topics";
+	topics.map((topic) => {
+		const tab = document.createElement("div");
+		tab.innerHTML = topic;
+		tab.className = "tab";
+		topicsHolder.appendChild(tab);
+	});
+	return topicsHolder;
+};
 
 const tabsAppender = (selector) => {
-  // TASK 4
-  // ---------------------
-  // Implement this function which takes a css selector as its only argument.
-  // It should obtain topics from this endpoint: `http://localhost:5000/api/topics` (test it in Postman/HTTPie!).
-  // Find the array of topics inside the response, and create the tabs using the Tabs component.
-  // Append the tabs to the element in the DOM that matches the selector passed to the function.
-  //
-}
+	setTimeout(() => {
+		axios.get("http://localhost:5000/api/topics").then((response) => {
+			document.querySelector(selector).appendChild(Tabs(response.data.topics));
+			console.log(response.data.topics);
+		});
+	}, 1000);
+};
 
-export { Tabs, tabsAppender }
+export { Tabs, tabsAppender };
